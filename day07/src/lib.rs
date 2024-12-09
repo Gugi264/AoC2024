088e96b867ccb89882f0887b6b46987af9d2a03f
Simon Guggi <simon.guggi@iaik.tugraz.at>
2024-12-09 15:10:00 +0100

    fixed error

--- day07/src/lib.rs
+++ day07/src/lib.rs
@@ -1,4 +1,5 @@
 use aoc_traits::AdventOfCodeDay;
+use itertools::concat;
 use itertools::repeat_n;
 use itertools::Itertools;
 use std::iter;
@@ -14,6 +15,7 @@ pub struct InputLine {
 pub enum Operators {
     ADD,
     MUL,
+    CONCAT,
 }
 
 #[derive(Default)]
@@ -56,10 +58,11 @@ impl AdventOfCodeDay for Solver {
                         match ops {
                             Operators::ADD => tmp_result += x.inputs[i + 1],
                             Operators::MUL => tmp_result *= x.inputs[i + 1],
+                            Operators::CONCAT => {}
                         }
-                        if tmp_result > x.result {
-                            false
-                        }
+                        // if tmp_result > x.result {
+                        //     cont
+                        // }
                     });
                     if tmp_result == x.result {
                         true
@@ -77,14 +80,60 @@ impl AdventOfCodeDay for Solver {
     }
 
     fn solve_part2(input: &Self::ParsedInput<'_>) -> Self::Part2Output {
-        todo!()
+        let ops = vec![Operators::ADD, Operators::MUL, Operators::CONCAT];
+        input
+            .iter()
+            .filter_map(|x| {
+                let combinations =
+                    repeat_n(ops.iter(), x.inputs.len() - 1).multi_cartesian_product();
+                let found = combinations.into_iter().find(|ops_vec| {
+                    let mut tmp_result = x.inputs[0];
+                    ops_vec.iter().enumerate().for_each(|(i, ops)| {
+                        match ops {
+                            Operators::ADD => tmp_result += x.inputs[i + 1],
+                            Operators::MUL => tmp_result *= x.inputs[i + 1],
+                            Operators::CONCAT => {
+                                tmp_result = concat_nr(tmp_result, x.inputs[i + 1]);
+                            }
+                        }
+                        // if tmp_result > x.result {
+                        //     cont
+                        // }
+                    });
+                    if tmp_result == x.result {
+                        true
+                    } else {
+                        false
+                    }
+                });
+                if found.is_some() {
+                    Some(x.result)
+                } else {
+                    None
+                }
+            })
+            .sum()
     }
+}
+
+pub fn concat_nr(a: usize, b: usize) -> usize {
+    a as usize * 10usize.pow(b.ilog10() + 1) + b as usize
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fs;
+
+    #[test]
+    fn test_concat() {
+        let a = 42;
+        let b = 32;
+        let c = 4232;
+
+        let reuslt = concat_nr(a, b);
+        assert_eq!(c, reuslt);
+    }
 
     #[test]
     fn test_part1() {
@@ -99,17 +148,17 @@ mod tests {
         let input = Solver::parse_input(&file_content);
         assert_eq!(Solver::solve_part1(&input), 7579994664753);
     }
-    // #[test]
-    // fn test_part2() {
-    //     let file_content = fs::read_to_string("src/test_puzzle.txt").expect("unable to read file");
-    //     let input = Solver::parse_input(&file_content);
-    //     assert_eq!(Solver::solve_part2(&input), 6);
-    // }
-    //
-    // #[test]
-    // fn solve_part2() {
-    //     let file_content = fs::read_to_string("src/puzzle.txt").expect("unable to read file");
-    //     let input = Solver::parse_input(&file_content);
-    //     assert_eq!(Solver::solve_part2(&input), 1523);
-    // }
+    #[test]
+    fn test_part2() {
+        let file_content = fs::read_to_string("src/test_puzzle.txt").expect("unable to read file");
+        let input = Solver::parse_input(&file_content);
+        assert_eq!(Solver::solve_part2(&input), 11387);
+    }
+
+    #[test]
+    fn solve_part2() {
+        let file_content = fs::read_to_string("src/puzzle.txt").expect("unable to read file");
+        let input = Solver::parse_input(&file_content);
+        assert_eq!(Solver::solve_part2(&input), 1523);
+    }
 }
